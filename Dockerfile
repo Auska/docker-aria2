981d4bf5083b591c151f032c140e57a7275500bd
Auska <luodan0709@live.cn>
2020-05-15 10:30:32 +0800

    add build.sh

--- Dockerfile
+++ Dockerfile
@@ -4,18 +4,11 @@ FROM alpine:3.11 as compilingaria2c
 
 ARG  ARIA2_VER=1.35.0
 
-RUN  apk add --no-cache ca-certificates make g++ gcc zlib-dev openssl-dev expat-dev sqlite-dev c-ares-dev libssh2-dev  \
-&&   mkdir /aria2c  \
-&&   wget -P /aria2c   https://github.com/aria2/aria2/releases/download/release-${ARIA2_VER}/aria2-${ARIA2_VER}.tar.gz  \
-&&   tar  -zxvf  /aria2c/aria2-${ARIA2_VER}.tar.gz  -C    /aria2c  \
-&&   cd  /aria2c/aria2-${ARIA2_VER}  \
-&&   sed  -i  's/"1", 1, 16/"1", 1, 128/g'          src/OptionHandlerFactory.cc    \
-&&   sed  -i  's/"20M", 1_m, 1_g/"4k", 1_k, 1_g/g'  src/OptionHandlerFactory.cc    \
-&&   ./configure --without-libxml2  --without-gnutls --with-openssl  --host=x86_64-alpine-linux-musl   \
-&&   make install-strip   \
-&&   ldd /usr/local/bin/aria2c   |cut -d ">" -f 2|grep lib|cut -d "(" -f 1|xargs tar -chvf /aria2c/aria2c.tar  \
-&&   mkdir /aria2  \
-&&   tar  -xvf /aria2c/aria2c.tar  -C /aria2   \
+# copy local files
+COPY  root /
+
+RUN  apk add --no-cache ca-certificates make g++ gcc wget bash \
+&&   bash /defaults/build.sh \
 &&   cp --parents /usr/local/bin/aria2c /aria2
 
 # docker aria2 
---
+++ root/defaults/build.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+# In this configuration, the following dependent libraries are compiled:
+#
+# * zlib
+# * c-ares
+# * expat
+# * sqlite3
+# * openSSL
+# * libssh2
+
+#COMPILER AND PATH
+mkdir -p /build/build_libs
+
+PREFIX=/build/build_libs
+C_COMPILER="x86_64-alpine-linux-musl-gcc"
+CXX_COMPILER="x86_64-alpine-linux-musl-g++"
+
+#CHECK TOOL FOR DOWNLOAD
+ DOWNLOADER="wget -c"
+
+## DEPENDENCES ##
+ZLIB=http://sourceforge.net/projects/libpng/files/zlib/1.2.11/zlib-1.2.11.tar.gz
+OPENSSL=https://www.openssl.org/source/openssl-1.1.1g.tar.gz
+EXPAT=https://github.com/libexpat/libexpat/releases/download/R_2_2_9/expat-2.2.9.tar.bz2
+SQLITE3=https://sqlite.org/2020/sqlite-autoconf-3310100.tar.gz
+C_ARES=https://github.com/c-ares/c-ares/releases/download/cares-1_16_1/c-ares-1.16.1.tar.gz
+SSH2=https://github.com/libssh2/libssh2/releases/download/libssh2-1.9.0/libssh2-1.9.0.tar.gz
+ARIA2=https://github.com/aria2/aria2/releases/download/release-${ARIA2_VER}/aria2-${ARIA2_VER}.tar.xz
+
+## CONFIG ##
+BUILD_DIRECTORY=/build
+
+## BUILD ##
+cd $BUILD_DIRECTORY
+#
+ # zlib build
+  $DOWNLOADER $ZLIB
+  tar zxvf zlib-1.2.11.tar.gz
+  cd zlib-1.2.11/
+  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC="$C_COMPILER" CXX="$CXX_COMPILER" ./configure --prefix=$PREFIX --static
+  make
+  make install
+#
+ # expat build
+  cd ..
+  $DOWNLOADER $EXPAT
+  tar jxvf expat-2.2.9.tar.bz2
+  cd expat-2.2.9/
+  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC="$C_COMPILER" CXX="$CXX_COMPILER" ./configure --prefix=$PREFIX --enable-static --enable-shared
+  make
+  make install
+#
+ # c-ares build
+  cd ..
+  $DOWNLOADER $C_ARES
+  tar zxvf c-ares-1.16.1.tar.gz
+  cd c-ares-1.16.1/
+  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC="$C_COMPILER" CXX="$CXX_COMPILER" ./configure --prefix=$PREFIX --enable-static --disable-shared
+  make
+  make install
+#
+ # Openssl build
+  cd ..
+  $DOWNLOADER $OPENSSL
+  tar zxvf openssl-1.1.1g.tar.gz
+  cd openssl-1.1.1g/
+  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC="$C_COMPILER" CXX="$CXX_COMPILER" ./Configure --prefix=$PREFIX linux-x86_64 shared
+  make
+  make install
+#
+ # sqlite3
+  cd ..
+  $DOWNLOADER $SQLITE3
+  tar zxvf sqlite-autoconf-3310100.tar.gz
+  cd sqlite-autoconf-3310100/
+  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC="$C_COMPILER" CXX="$CXX_COMPILER" ./configure --prefix=$PREFIX --enable-static --enable-shared
+  make
+  make install
+#
+ # libssh2
+  cd ..
+  $DOWNLOADER $SSH2
+  tar zxvf libssh2-1.9.0.tar.gz
+  cd libssh2-1.9.0/
+  rm -rf $PREFIX/lib/pkgconfig/libssh2.pc
+  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ LD_LIBRARY_PATH=$PREFIX/lib/ CC="$C_COMPILER" CXX="$CXX_COMPILER" ./configure --without-libgcrypt --with-openssl --without-wincng --prefix=$PREFIX --enable-static --disable-shared
+  make
+  make install
+#
+ # aria2
+  cd ..
+  $DOWNLOADER $ARIA2
+  tar xf aria2-${ARIA2_VER}.tar.xz
+  cd aria2-${ARIA2_VER}/
+  sed -i 's/"1", 1, 16/"1", 1, 128/g' src/OptionHandlerFactory.cc
+  PKG_CONFIG_PATH=$PREFIX/lib/pkgconfig/ \
+  LD_LIBRARY_PATH=$PREFIX/lib/ \
+  ./configure \
+    --without-libxml2 \
+    --without-libgcrypt \
+    --with-openssl \
+    --without-libnettle \
+    --without-gnutls \
+    --without-libgmp \
+    --with-libssh2 \
+    --with-sqlite3 \
+    --with-ca-bundle='/etc/ssl/certs/ca-certificates.crt' \
+    ARIA2_STATIC=yes \
+    --enable-shared=no
+    make install-strip
+#
+ #cleaning
+  cd ..
+  rm -rf c-ares*
+  rm -rf sqlite-autoconf*
+  rm -rf zlib-*
+  rm -rf expat-*
+  rm -rf openssl-*
+  rm -rf libssh2-*
+  rm -rf aria2-*
+#
+ echo "finished!"
